543dec8b974b07eeed447828cdc513e1fd0bbe8c
evvanted <evvanted@gmail.com>
2019-06-30 20:01:07 +0300

    begin blog page add articlesController

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,9 +2,39 @@
 
 namespace Corp\Http\Controllers;
 
+use Corp\Repositories\ArticlesRepository;
+use Corp\Repositories\PortfoliosRepository;
 use Illuminate\Http\Request;
 
-class ArticlesController extends Controller
+class ArticlesController extends SiteController
 {
     //
+    public function __construct(PortfoliosRepository $p_rep, ArticlesRepository $a_rep){
+        parent::__construct(new \Corp\Repositories\MenusRepository(new \Corp\Menu));
+        $this->p_rep = $p_rep;
+        $this->a_rep = $a_rep;
+        $this->bar = 'right';
+        $this->template = env('THEME').'.articles';
+    }
+
+    public function index()
+    {
+        //
+        $articles = $this->getArticles();
+
+
+        return $this->renderOutput();
+    }
+
+    public function getArticles($alias = false){
+        $articles = $this->a_rep->get(['title','alias','created_at','img','desc'],false,true);
+//        dd($articles);
+        if($articles){
+//            $articles->load('user','category','comments');
+        }
+
+        return $articles;
+
+    }
+
 }
