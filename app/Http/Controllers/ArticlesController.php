fa9a54c27416df8bfdb1129287942e16087a7339
evvanted <evvanted@gmail.com>
2019-07-06 22:40:13 +0300

    lesson69 rightbar comments

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -41,7 +41,7 @@ class ArticlesController extends SiteController
         $articles = $this->a_rep->get(['id','title','alias','created_at','img','desc','user_id','category_id'],false,true);
 //        dd($articles);
         if($articles){
-//            $articles->load('user','category','comments');
+            $articles->load('user','category','comments');
         }
 
         return $articles;
@@ -50,6 +50,9 @@ class ArticlesController extends SiteController
 
     public function getComments($take){
         $comments = $this->c_rep->get(['text','name','email','site','article_id','user_id'],$take);
+        if($comments){
+            $comments->load('user','article');
+        }
         return $comments;
     }
 
