2dbbe50641249894cbb7ffa5902ca2aeeb47d94c
evvanted <evvanted@gmail.com>
2021-06-05 17:35:31 +0300

    fixes

--- database/migrations/2019_08_12_095239_change__articles__table2.php
+++ database/migrations/2019_08_12_095239_change__articles__table2.php
@@ -14,8 +14,8 @@ class ChangeArticlesTable2 extends Migration
     public function up()
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->string('keywords');
-            $table->string('meta_desc');
+            $table->string('keywords')->nullable();
+            $table->string('meta_desc')->nullable();
             //
         });
     }
